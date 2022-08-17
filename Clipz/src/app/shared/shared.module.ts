fa2a8aadc5aa575f2536165f0df9fbaf498aeef4
Igor Pustovoy <igorpustovoy1978@gmail.com>
2022-08-17 08:26:58 +0200

    Reactive forms

--- Clipz/src/app/shared/shared.module.ts
+++ Clipz/src/app/shared/shared.module.ts
@@ -1,24 +1,34 @@
+import { ReactiveFormsModule } from '@angular/forms';
 // import { ModalService } from './../services/modal.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { NgxMaskModule } from 'ngx-mask';
+import { AlertComponent } from './alert/alert.component';
 
 
 @NgModule({
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent,
+    AlertComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot()
   ],
   exports : [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent,
+    AlertComponent
   ],
   // providers: [ModalService]
 })
--- Clipz/src/app/user/register/register.component.ts
+++ Clipz/src/app/user/register/register.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  showAlert = false;
+  alertMsg = 'Please wait! Your account is being created.';
+  alertColor = 'blue';
 
-  constructor() { }
+  
+  registerForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    age: new FormControl('', [
+      Validators.required,
+      Validators.min(18),
+      Validators.max(120)
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
+    ]),
+    confirm_password: new FormControl('', [
+      Validators.required,
+    ]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(11),
+      Validators.maxLength(11)
+    ]),
+  });
 
-  ngOnInit(): void {
+  register() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! Your account is being created.';
+    this.alertColor = 'blue';
   }
-
 }
